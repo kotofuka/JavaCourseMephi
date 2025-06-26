3599493b68db48310edc0999fd907ad9d09cfd1e
kotofuka <hollidaymars@gmail.com>
2025-06-26 21:20:28 +0300

    some fixes in application file

--- lab4/src/main/java/com/mephi/lab4/Lab4Application.java
+++ lab4/src/main/java/com/mephi/lab4/Lab4Application.java
@@ -10,41 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Lab4Application {
 
     public static void main(String[] args) {
-//        SpringApplication.run(Lab4Application.class, args);
-
-        var app = SpringApplication.run(Lab4Application.class, args);
-
-        var beanUserService = app.getBean(UserService.class);
-
-//        beanUserService.addNewUser(User.builder()
-//                    .firstName("Oleg")
-//                    .lastName("Ivanov")
-//                    .role(Role.ADMIN)
-//                .build());
-//
-//        beanUserService.addNewUser(User.builder()
-//                .firstName("Maxim")
-//                .lastName("Bobrov")
-//                .role(Role.DEVELOPER)
-//                .build());
-//
-//        beanUserService.addNewUser(User.builder()
-//                .firstName("Vadim")
-//                .lastName("Uhov")
-//                .role(Role.SYSTEM_ANALYST)
-//                .build());
-//
-//        beanUserService.addNewUser(User.builder()
-//                .firstName("Maria")
-//                .lastName("Glazkova")
-//                .role(Role.TESTER)
-//                .build());
-//
-//        beanUserService.addNewUser(User.builder()
-//                .firstName("Eduard")
-//                .lastName("Tihonov")
-//                .role(Role.TEAM_LEAD)
-//                .build());
+        SpringApplication.run(Lab4Application.class, args);
     }
 
 }
